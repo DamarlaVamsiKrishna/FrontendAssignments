86f5259193c2b469e43e5ae52bba28c8c67793b1
DamarlaVamsiKrishna <damarla.vamsikrishna@thoughtfocus.com>
2022-01-11 08:11:18 +0530

    assessment

--- Assessment/js/view.js
+++ Assessment/js/view.js
@@ -5,40 +5,57 @@ let year=document.getElementById("year");
 year.textContent=date.getFullYear();
 
 let showtime=document.getElementById("showtime");
+let seconds=document.getElementById("seconds");
 
-let current_date=`Date :${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} Time : ${ date.toLocaleString('en-US', { hour: 'numeric', hour12: true })}`
+function sec(){
+    seconds.innerHTML=new Date().getSeconds();
+}
+setInterval(sec,1000)
 
+let current_date=`Date :${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} 
+Time : ${ date.getHours()}:${date.getMinutes()}:`
 showtime.innerHTML=current_date;
 
 
-
+let buttonIds=[];
+var editButton;
 
 function createRow(employee,i){
-    let tr=document.createElement("tr");
-    tableid.appendChild(tr);
+    let tr=document.createElement("tr"); 
+    tableid.appendChild(tr);                
     tr.classList.add("tabledata");
     
-    let slno= document.createElement("td");
+    let slno= document.createElement("td");  
     let id= document.createElement("td");
     let name= document.createElement("td");
     let department= document.createElement("td");
     let skill= document.createElement("td");
     let salary= document.createElement("td");
+    editButton =document.createElement("button");
+
+    editButton.textContent="Edit";
+    editButton.classList.add("editBtn")
+    editButton.setAttribute("id","button"+i);
+    buttonIds.push(editButton.id)
     
-    tr.appendChild(slno);
+    
+    tr.appendChild(slno);          
     tr.appendChild(id);
     tr.appendChild(name);
     tr.appendChild(department);
     tr.appendChild(skill);
     tr.appendChild(salary);
 
-    slno.textContent=i;
+    tr.appendChild(editButton);
+
+    slno.textContent=i;        
     id.textContent=employee.Eid;
     name.textContent=employee.name;
     department.textContent=employee.department;
     skill.textContent=employee.skill;
     salary.textContent=employee.salary;
 }
+
 
 let  keys = Object.keys(localStorage);
 let i=1;
@@ -52,4 +69,14 @@ for (let id of keys){
         createRow(parsed_object,i);
         i=i+1;
     }
+}
+console.log(buttonIds);
+
+addEventListener
+
+for (btn of buttonIds){
+    /*btn.addEventListener("click",(event)=>{
+        console.log(btn);
+    })*/
+    console.log(btn)
 }
